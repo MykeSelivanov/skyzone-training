39796035ad51de0bf89e490b9e488eac4cb9f292
MykeSelivanov <michael777selivanov@gmail.com>
2021-05-17 23:40:28 -0500

    draw pixels

--- js-projects/drawPixels/script.js
+++ js-projects/drawPixels/script.js
@@ -1,1 +1,46 @@
-// your code
+const table = document.getElementsByTagName('table')[0];
+const addRowBtn = document.getElementById('add-row');
+const select = document.getElementsByTagName('select')[0];
+
+let chosenColor = 'red';
+
+const makeRow = () => {
+    const row = document.createElement('tr');
+    for (let idx = 0; idx < 20; idx++) {
+        const td = document.createElement('td');
+        row.appendChild(td);
+    }
+    table.appendChild(row);
+}
+
+const colorize = (event) => {
+    const target = event.target;
+    if(target.tagName !== 'TD') {
+        return;
+    }
+    if(target.className === chosenColor) {
+        target.className = '';
+    } else {
+        target.className = chosenColor;
+    }
+}
+
+const pickColor = (event) => {
+    chosenColor = event.target.value;
+}
+
+const addMouseOver = () => {
+    table.addEventListener('mouseover', colorize)
+}
+
+const removeMouseOver = () => {
+    table.removeEventListener('mouseover', colorize);
+}
+
+addRowBtn.addEventListener('click', makeRow);
+table.addEventListener('click', colorize);
+select.addEventListener('change', pickColor);
+table.addEventListener('mousedown', addMouseOver);
+table.addEventListener('mouseup', removeMouseOver);
+
+
