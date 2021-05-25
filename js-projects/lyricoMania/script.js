29d8036ba803d671030e6f6458c8093553284afa
MykeSelivanov <michael777selivanov@gmail.com>
2021-05-24 21:35:57 -0500

    lyrico mania

--- js-projects/lyricoMania/script.js
+++ js-projects/lyricoMania/script.js
@@ -1,1 +1,23 @@
-const searchBtn = document.getElementsByTagName('button');
+// get the elements
+const searchBtn = document.querySelector('button');
+const searchInput = document.querySelector('input');
+
+//serch songs function
+async function searchSongs(input) {
+    let response = await fetch(`https://api.lyrics.ovh/suggest/${input}`);
+    let result = await response.json();
+    return result.data;
+}
+
+// search lyrics
+searchBtn.addEventListener('click', async (event) => {
+    event.preventDefault();
+
+    const userInput = searchInput.value;
+    if(!userInput) {
+        alert('Plese provide an input');
+    } else {
+        let data = await searchSongs(userInput);
+        console.log(data);
+    }
+});
