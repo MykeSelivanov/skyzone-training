2f248fcc18fad852b7360217d74149b0dbd58f29
MykeSelivanov <michael777selivanov@gmail.com>
2021-05-25 21:34:55 -0500

    chuck practice

--- node-projects/chuck-norris/index.js
+++ node-projects/chuck-norris/index.js
@@ -10,16 +10,22 @@ app.set('view engine', 'handlebars');
 
 // some data
 let foods = ['avocado', 'broccoli', 'carrots', 'apple'];
+let songs = ['Good 4 U', 'Sweet Melody', 'More Than My Hometown', 'Old Town Road', 'Blinding Lights', 'Rain on Me'];
 
 // routes
 app.get('/', (req, res) => {
     let msg = 'hi';
+    let errMsg = "<h1>there's been an error</h1>";
     const item1 = foods[0];
     res.render('home', {item1, foods});
+});
+
+app.get('/songList', (req, res) => {
+    res.render('song_list', {songs});
 });
 
 
 // start server
 app.listen(5050, () => {
     console.log('app is running on localhost:/5050');
-})
+});
