7e29fbb72a1796cca8a6fd164baac707a3424d23
MykeSelivanov <michael777selivanov@gmail.com>
2021-05-28 22:04:25 -0500

    adding path

--- node-projects/simple-node-express-tutorial/index.js
+++ node-projects/simple-node-express-tutorial/index.js
@@ -1,12 +1,15 @@
 // import modules
 const express = require('express');
+const path = require('path');
 
 // creating app/server
 const app = express();
 
+console.log(path.join(__dirname, "public"));
+
+
 // use the port from provided environment, in case it's undefined, run the app on 3000
 const PORT = process.env.PORT || 3000;
-
 
 // listen
 app.listen(PORT, () => {
