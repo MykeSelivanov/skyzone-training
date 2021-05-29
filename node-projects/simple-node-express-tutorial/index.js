1ecc3739ff2108b018cb17533ad4841505795cc6
MykeSelivanov <michael777selivanov@gmail.com>
2021-05-28 22:06:17 -0500

    adding route

--- node-projects/simple-node-express-tutorial/index.js
+++ node-projects/simple-node-express-tutorial/index.js
@@ -5,8 +5,12 @@ const path = require('path');
 // creating app/server
 const app = express();
 
-console.log(path.join(__dirname, "public"));
+// console.log(path.join(__dirname, "public"));
 
+// routes
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
 
 // use the port from provided environment, in case it's undefined, run the app on 3000
 const PORT = process.env.PORT || 3000;
