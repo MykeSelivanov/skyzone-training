c9dac7f0ef803b0a396f6277995ff7ac8f9dddb0
MykeSelivanov <michael777selivanov@gmail.com>
2021-05-28 22:14:02 -0500

    route updates

--- node-projects/simple-node-express-tutorial/index.js
+++ node-projects/simple-node-express-tutorial/index.js
@@ -4,13 +4,34 @@ const path = require('path');
 
 // creating app/server
 const app = express();
+const students = [
+    {
+        id: 1,
+        name: "Max"
+    },
+    {
+        id: 2,
+        name: "Peter"
+    },
+    {
+        id: 3,
+        name: "Mike"
+    },
+    {
+        id: 4,
+        name: "Umar"
+    },
+];
 
+// getting directory path
 // console.log(path.join(__dirname, "public"));
 
 // routes
 app.get('/', (req, res) => {
-    res.send('Hello World');
-})
+    // res.send("<h1>Hello World</h1>");
+    // res.sendFile(__dirname + "/public/" + "index.html");
+    res.json(students);
+});
 
 // use the port from provided environment, in case it's undefined, run the app on 3000
 const PORT = process.env.PORT || 3000;
